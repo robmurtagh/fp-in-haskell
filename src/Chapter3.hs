968ab7169e5bf9db10fc828a9e9fc1d18bb214ce
Rob Murtagh <robmurtagh@googlemail.com>
2019-01-25 17:08:37 +0000

    Added Chapter 3 tree data type

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -3,6 +3,11 @@ module Chapter3
     )
 where
 
+
+--------------------------------------------------
+-- LIST STYLE DATA STRUCTURE
+--------------------------------------------------
+
 data MyList a = Empty
     | Cons a (MyList a)
     deriving (Show, Eq)
@@ -30,11 +35,49 @@ myDrop (Cons x xs) n = myDrop xs (n - 1)
 
 myDropWhile :: MyList a -> (a -> Bool) -> MyList a
 myDropWhile Empty f = Empty
-myDropWhile (Cons x xs) f
-    | f(x) = myDropWhile xs f
-    | otherwise = Cons x xs
+myDropWhile (Cons x xs) f | f x       = myDropWhile xs f
+                          | otherwise = Cons x xs
 
 myInit :: MyList a -> MyList a
-myInit Empty = Empty
+myInit Empty                   = Empty
 myInit (Cons x (Cons y Empty)) = Cons x Empty
-myInit (Cons x xs) = Cons x (myInit xs)
+myInit (Cons x xs            ) = Cons x (myInit xs)
+
+myFoldRight :: MyList a -> b -> (a -> b -> b) -> b
+myFoldRight Empty       b _ = b
+myFoldRight (Cons a as) b f = f a (myFoldRight as b f)
+
+myLength :: MyList a -> Int
+myLength Empty = 0
+myLength as    = myFoldRight as 0 (\a b -> b + 1)
+
+myFoldLeft :: MyList a -> b -> (b -> a -> b) -> b
+myFoldLeft Empty       b _ = b
+myFoldLeft (Cons a as) b f = myFoldLeft as (f b a) f
+
+myReverse :: MyList a -> MyList a
+myReverse xs = myFoldLeft xs Empty (\b a -> Cons a b)
+
+
+--------------------------------------------------
+-- BINARY TREE STYLE DATA STRUCTURE
+--------------------------------------------------
+
+data MyTree a = Leaf a | Branch (MyTree a) (MyTree a) | DeadLeaf
+    deriving (Show, Eq)
+
+mySize :: MyTree a -> Int
+mySize (Leaf _) = 1
+mySize (Branch left right) = mySize(left) + mySize(right)
+
+myMax :: (Ord a) => MyTree a -> a
+myMax (Leaf a) = a
+myMax (Branch left right) = max (myMax left) (myMax right)
+
+myDepth :: MyTree a -> Int
+myDepth (Leaf _) = 0
+myDepth (Branch left right) = max ((myDepth left) + 1) ((myDepth right) + 1)
+
+myMap :: MyTree a -> (a -> b) -> MyTree b
+myMap (Leaf a) f = Leaf (f a)
+myMap (Branch left right) f = Branch (myMap left f) (myMap right f)
--- test/Chapter3Spec.hs
+++ test/Chapter3Spec.hs
@@ -24,8 +24,31 @@ spec = describe "MyList" $ do
     $          myDrop (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty))))) 2
     `shouldBe` (Cons 3 (Cons 4 (Cons 5 Empty)))
   it "should have a working dropWhile function"
-    $          myDropWhile (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty))))) (\x -> x <=3)
+    $          myDropWhile (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty)))))
+                           (\x -> x <= 3)
     `shouldBe` (Cons 4 (Cons 5 Empty))
   it "should have a working init function"
     $          myInit (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty)))))
     `shouldBe` (Cons 1 (Cons 2 (Cons 3 (Cons 4 Empty))))
+  it "should have a working foldRight function"
+    $ myFoldRight (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty))))) 0 (+)
+    `shouldBe` (15 :: Int)
+  it "should have a working length function implemented using foldRight"
+    $          myLength (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty)))))
+    `shouldBe` (5 :: Int)
+  it "should have a working reverse function implemented using a fold"
+    $          myReverse (Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 Empty)))))
+    `shouldBe` (Cons 5 (Cons 4 (Cons 3 (Cons 2 (Cons 1 Empty)))))
+
+  describe "MyTree" $ do
+    it "should be constructable"
+      $          Branch (Leaf 1) (Branch (Leaf 2) (Leaf 3)) `shouldBe` Branch (Leaf 1) (Branch (Leaf 2) (Leaf 3))
+    it "should have a working size function"
+      $          mySize(Branch (Leaf 1) (Branch (Leaf 2) (Leaf 3))) `shouldBe` (3 :: Int)
+    it "should have a working max function"
+      $          myMax(Branch (Leaf (-15)) (Branch (Leaf 223) (Leaf 33))) `shouldBe` (223 :: Int)
+    it "should have a working depth function"
+      $          myDepth(Branch (Leaf (-15)) (Branch (Leaf 223) (Branch (Leaf 45) (Leaf 33)))) `shouldBe` (3 :: Int)
+    it "should have a working map function"
+      $          myMap(Branch (Leaf (-15)) (Branch (Leaf 223) (Branch (Leaf 45) (Leaf 33)))) ((+) 1) 
+      `shouldBe` Branch (Leaf (-14)) (Branch (Leaf 224) (Branch (Leaf 46) (Leaf 34)))
