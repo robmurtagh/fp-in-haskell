2bb96fdb6479a6faeba1f1d712c46cdd5c056fa1
Rob Murtagh <robmurtagh@googlemail.com>
2019-02-01 18:48:19 +0000

    More exercises in Maybe

--- src/Chapter4.hs
+++ src/Chapter4.hs
@@ -11,9 +11,43 @@ data MyOption a = Some a | None
     deriving (Show, Eq)
 
 myMap :: MyOption a -> (a -> b) -> MyOption b
-myMap None _ = None
+myMap None     _ = None
 myMap (Some a) f = Some (f a)
 
 myFlatMap :: MyOption a -> (a -> MyOption b) -> MyOption b
-myFlatMap None _ = None
+myFlatMap None     _ = None
 myFlatMap (Some a) f = f a
+
+myGetOrElse :: MyOption a -> a -> a
+myGetOrElse None     dflt = dflt
+myGetOrElse (Some a) _    = a
+
+myOrElse :: MyOption a -> MyOption a -> MyOption a
+myOrElse None     dflt = dflt
+myOrElse (Some a) _    = Some a
+
+myFilter :: MyOption a -> (a -> Bool) -> MyOption a
+myFilter None _ = None
+myFilter (Some a) f | f a       = Some a
+                    | otherwise = None
+
+myMean :: (Fractional a) => [a] -> MyOption a
+myMean [] = None
+myMean xs = Some $ (foldl (+) 0 xs) / (foldl (\b a -> b + 1) 0 xs)
+
+myVariance :: (Fractional a) => [a] -> MyOption a
+myVariance [] = None
+myVariance xs =
+    myFlatMap (myMean xs) (\m -> myMean (map (\x -> (x - m) ^ 2) xs))
+
+-- If either return None, this returns None
+myMap2 :: MyOption a -> MyOption b -> (a -> b -> c) -> MyOption c
+myMap2 a b f = myFlatMap a (\aa -> myMap b (\bb -> f aa bb))
+
+mySequence :: [MyOption a] -> MyOption [a]
+mySequence [] = Some []
+mySequence xs = foldr (\b a -> myMap2 b a (:)) (Some []) xs
+
+myTraverse :: [a] -> (a -> MyOption b) -> MyOption [b]
+myTraverse []       _ = Some []
+myTraverse (x : xs) f = myMap2 (f x) (myTraverse xs f) (:)
--- test/Chapter4Spec.hs
+++ test/Chapter4Spec.hs
@@ -5,9 +5,35 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "MyOption" $ do
-    it "should be constructable"
-        $ Some "Hello" `shouldBe` Some "Hello"
+    it "should be constructable" $ Some "Hello" `shouldBe` Some "Hello"
     it "should map with None handling"
-        $ myMap None (\x -> x + 1) `shouldBe` None
+        $          myMap None (\x -> x + 1)
+        `shouldBe` None
     it "should map over Some value"
-        $ myMap (Some 11) (\x -> x + 1) `shouldBe` Some 12
+        $          myMap (Some 11) (\x -> x + 1)
+        `shouldBe` Some 12
+    it "should have a getOrElse accessor handling Some"
+        $          myGetOrElse (Some 11) 12
+        `shouldBe` 11
+    it "should have a getOrElse accessor handling None"
+        $          myGetOrElse None 12
+        `shouldBe` 12
+    it "should have a working filter function"
+        $          myFilter (Some 5) ((==) 5)
+        `shouldBe` Some 5
+    it "should have a working filter function"
+        $          myFilter (Some 5) ((==) 4)
+        `shouldBe` None
+    it "should have a variance implementation using flatMap"
+        $          myVariance [1, 5, 7, 3, 4, 4, 2, 3, 5, 7, 4]
+        `shouldBe` Some 3.173553719008264
+    it "should have a map2 implementation"
+        $          myMap2 (Some 5) (Some 6) (+)
+        `shouldBe` Some 11
+    it "should have a sequence implementation"
+        $          mySequence [(Some 5), (Some 6), (Some 7)]
+        `shouldBe` Some [5, 6, 7]
+    it "should have a traverse implementation"
+        $          myTraverse [1, 2, 3, 4, 5] (\x -> Some (x + 1))
+        `shouldBe` Some [2, 3, 4, 5, 6]
+
